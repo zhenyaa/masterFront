55af9071f5fdb2e303bd9d53a9db8f97ed81d9e0
zhenyaa <zhenya_kleshov@inbox.ru>
2018-12-03 20:42:04 +0300

    1

--- src/app/record/record.module.ts
+++ src/app/record/record.module.ts
@@ -5,12 +5,16 @@ import { RecordRoutingModule } from './record-routing.module';
 import { ShowRecordComponent } from './show-record/show-record.component';
 import { AddRecordComponent } from './add-record/add-record.component';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [ShowRecordComponent, AddRecordComponent],
   imports: [
     CommonModule,
     RecordRoutingModule,
-    MatTabsModule
+    MatTabsModule,
+    MatListModule,
+    MatCardModule
   ]
 })
 export class RecordModule { }
